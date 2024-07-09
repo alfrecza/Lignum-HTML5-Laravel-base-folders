6f26b2cd9c3ec9acc55819ef57baa0468723fcb9
Alfre Chazarreta <alfre.czta@gmail.com>
2024-07-09 13:01:38 -0300

    01.4 & 01.6: Resolución de ejercicios.

--- 01-JS-DOM-APIs/app.js
+++ 01-JS-DOM-APIs/app.js
@@ -1,5 +1,6 @@
 const boton = document.querySelector('.button');
 const boton2 = document.querySelector('.button2');
+const formRepo = document.querySelector('.formRepo')
 
 const config = {
     method: "GET",
@@ -18,8 +19,22 @@ function addEventListeners () {
             chuckNorrisRender({ error: 'Ocurrió un error al obtener los datos' });
         }
     });
-};
-
+    formRepo.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const error = document.createElement('div')
+        if(e.target[0].value.length <= 0) {
+            error.innerHTML = `<p class="error">${'Debes ingresar un criterio de busqueda'}</p>`
+            formRepo.appendChild(error)
+            setTimeout(() => {
+                formRepo.removeChild(error)
+            }, 3000);
+            return
+        }
+        fetchRepositories(e.target[0].value)
+        formRepo.reset()
+        
+    });
+}
 const touchButton = () => {
     alert("Tocaste el boton!");
 };
@@ -66,8 +81,97 @@ async function chuckNorrisRender (data) {
         <p>${isError ? data.error : data.value}</p>
         
     `
-    
 };
+
+async function fetchRepositories (data) {
+    
+    if(document.querySelector('.repositories') !== null) {
+        document.querySelector('main').removeChild(document.querySelector('.repositories'));
+    }
+
+    const sectionRepositories = document.createElement('section');
+
+    //Crear la seccion para renderizar los elementos
+    sectionRepositories.classList.add('repositories');
+    document.querySelector('.main').insertBefore(sectionRepositories, document.querySelector('div1'));
+
+
+    //realizar el req a la api
+    const url = `https://api.github.com/search/repositories?q=${data}`;
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+
+    //Estructurar el html
+    const ul = document.createElement('ul');
+    sectionRepositories.appendChild(ul);
+
+    resultado.items.forEach(r => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <p>Titulo: ${r.name}</p>
+            <p>Descripcion: ${r.description}</p>
+            <p>Dueño: ${r.owner.login}</p>
+        `
+        ul.appendChild(li);
+    })
+
+    document.querySelector('.div1').classList.remove('w-100');
+    document.querySelector('.div1').classList.add('w-70');
+    document.querySelector('.div1').classList.add('mr-2');
+    
+
+}
+
+const data = [
+    ["Nombre", "Edad", "Ciudad"],
+    ["Juan", 25, "Madrid"],
+    ["Ana", 30, "Barcelona"],
+    ["Luis", 28, "Valencia"]
+];
+
+
+createTable(data)
+function createTable(data) {
+    const tabla = document.createElement('table');
+    tabla.classList.add('table');
+
+    //Head
+
+    const thead = document.createElement('thead');
+    const trHead = document.createElement('tr');
+
+    data[0].forEach(d => {
+        const th = document.createElement('th');
+        const textoTh = document.createTextNode(d);
+        th.appendChild(textoTh);
+        trHead.appendChild(th);
+    })
+
+    thead.appendChild(trHead);
+    tabla.appendChild(thead);
+
+
+    //Table body
+    const tbody = document.createElement('tbody');
+    
+    const tBodyData = data.slice(1)
+    tBodyData.forEach(d => {
+        const tr = document.createElement('tr')
+        d.forEach((bodyData) => {
+            const td = document.createElement('td');
+            const bodyTableText = document.createTextNode(bodyData);
+            td.appendChild(bodyTableText);
+            tr.appendChild(td);
+        })
+        tbody.appendChild(tr);
+    })
+
+    tabla.appendChild(tbody);
+    tabla.classList.add('tableStyles');
+    document.querySelector('.div1').appendChild(tabla);
+    
+}
+
 
 
 
